6a23a7ca9353a1a456448c8791581fa9216d8b52
Tim Savolainen <timsavolainen@me.com>
2014-02-09 12:46:24 -0500

    Trying to fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Pinteresting::Application.routes.draw do
 
   devise_for :users
   root "pins#index"
-  get "about" => "pages#about"
+  get "about" => "pages#about" # creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
