fcc9aa804524d1095e54cd43d4b44ecc0355fb8c
Tim Savolainen <timsavolainen@me.com>
2014-10-05 16:02:56 -0400

    change photo sizing

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }	
+	has_attached_file :image, :styles => { :medium => "1200x1200>", :thumb => "300x300>" }	
 
 	validates :description, presence: true
 	validates :image, presence: true
